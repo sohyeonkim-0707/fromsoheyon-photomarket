59f15e7ce4b3bdc6586787aa46806e75050a5836
sohyeon <hjthgus777@gmail.com>
2022-07-28 16:01:48 +0900

    login

--- src/commons/apollo/index.tsx
+++ src/commons/apollo/index.tsx
@@ -1,4 +1,4 @@
-]import {
+import {
   ApolloProvider,
   ApolloClient,
   ApolloLink,
--- src/components/units/login/Login.validation.ts
+++ src/components/units/login/Login.validation.ts
@@ -4,7 +4,7 @@ export const schema = yup.object({
   email: yup
     .string()
     .email("이메일 아이디를 @까지 정확하게 입력해주세요 .")
-    .required("이메일은 필수 입력 !!"),
+    .required("이메일은 필수 입력사항입니다."),
   password: yup
     .string()
     .max(16, "영문+숫자조합 8~16자리의 비밀번호 입력해주세요.")
